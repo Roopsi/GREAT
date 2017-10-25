8ab3a474a19a7ae7cb8aecb80fb51dfeced568c5
Roopa <roopa.h@egovernments.org>
2017-10-25 17:00:54 +0530

    Notification test

--- TestDemo/src/main/java/org/test/TestDemo/TestDemoApplication.java
+++ TestDemo/src/main/java/org/test/TestDemo/TestDemoApplication.java
@@ -3,6 +3,9 @@ package org.test.TestDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Author Roopsi
+ */
 @SpringBootApplication
 public class TestDemoApplication {
 
